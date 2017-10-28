3310b3634c44cac57d0c9337264108adfbc2a052
teeedor <teeedor@DESKTOP-8GM4LO8>
2017-10-28 16:47:24 -0700

    added custom input to collatz conjecture as well as cleaned it up and aligned all comments

--- collatz.py
+++ collatz.py
@@ -1,68 +1,68 @@
-#Collatz
-
-#3n+1
+import random
 import time
-#Take in an input integer or choose to pick a random
-#-1 = a choose random integer
 
-#Be able to choose how in depth you want to show the documentation
-    #First level: Just show the execution time and number of iterations
-    #Second Level: Show the numbers of each iteration
-    #Third Level: Show the Operation taken
+#Collatz Conjecture
+    #This program will take in an input integer
+    #or generate a random int
+    #from 1 to 10,000
+    #Choose random int with input value of -1
+
 #---------------------------------------------------------------------
+#INPUT
+Test_Num = input('Enter your input value: ')
 
-#Example read in input
-#raw_input('Enter your input value')
-
+if int(Test_Num) == -1:                         #Checks for random value generation
+    Test_Num = random.randint(1,10000)          #Generate random number
+    print('Random Number Chosen: ', Test_Num)
+else:
+    print('Chosen Number: ',Test_Num)           #Prints the number that has been input
+#---------------------------------------------------------------------
+#COLLATZFUNC FUNCTION DEFINITION
 Test1 = False
-Test2 = True
+Test2 = False
 Test3 = True
-waitTime = .05
-
-
-#try:
-#    mode = int(raw_input('Choose a number to operate on (-1 for random number):'))
-#except ValueError:
-#    print('choose a correct number')
-#
-#print(mode)
+waitTime = .2                                   #Amount of time to wait before executing next operation
 
 def CollatzFunc(input):
-    temp1=input                 #this is what the operation will take place on
-    num_operations = 0
+    temp1= int(input)
+    num_operations = 0                          #counter for the total number of Operations
+    num_even = 0                                #To keep track of even numbers
+    num_other = 0                               #To keep track of Odds
 
-    while temp1 != 1.0:
-        if temp1 % 2 == 0:           #If the value is even
+    if Test2:                                   #Test if the program gets to here
+        print('BEFORE THE FOR LOOP')
+
+    while temp1 != 1.0:                         #Base case to drop out of the loop
+
+        if Test2:                               #Test if the program gets to here
+            print('INSIDE THE FOR LOOP')
+
+        if temp1 % 2 == 0:                      #If the value is even
             temp1 = temp1/2
             num_operations += 1
-            print(temp1)
+            num_even += 1
+            print(int(temp1))
             if(Test1):
-                #Print the
                 print('Divide by 2\n')
                 print('Current Number of Operations: ',num_operations)
-            #if(Test2):
-                #Some Comment
-            #if(Test3):
-                #Some Comment
+
             time.sleep(waitTime)
-        else:
-            #multiply by 3 and add 1
-            temp1 = ((temp1 * 3) + 1)
-            num_operations += 1
-            print(temp1)
+        else:                                   #The Number is not even
+            temp1 = ((temp1 * 3) + 1)           #Multiply by 3 and add 1
+            num_operations += 1                 #increment Counters
+            num_other += 1
+            print(int(temp1))
             if(Test1):
-                #Some Comment
                 print('Multiply by 3, add 1\n')
                 print('Current Number of Operations: ',num_operations)
-            #if(Test2):
-                #Some Comment
-            #if(Test3):
-                #Some Comment
-            time.sleep(waitTime)
-    #Outside of the while loop
+            time.sleep(waitTime)                #make the program wait so we can watch it execute
 
+    #Outside of the while loop
+    print('Starting Number: ',Test_Num)
     print('Total Number of Operations: ',num_operations)
-    #Maybe add functionality to show how long it took to do
+    print('Number of Even Ops: ', num_even)
+    print('number of Other Ops: ', num_other)
+#---------------------------------------------------------------------
 
 #Function Call
-CollatzFunc(1005)
+CollatzFunc(Test_Num)
